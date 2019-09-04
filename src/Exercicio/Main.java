0411e0d65d77b5281e78c63c7d5e50a0ef6f791e
João Rafael <jmcr96@hotmail.com>
2019-09-04 10:41:20 +0100

    Initial commit

---
+++ src/Exercicio/Main.java
@@ -0,0 +1,14 @@
+package Exercicio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		People person1 = new People("João", "Rafael", 23);
+		People person2 = new People();
+		
+		
+		System.out.println(person1.toString());
+	}
+
+}
